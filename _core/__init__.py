fa533c4a8b6ec86e76cd67b49016925115bc8a22
Skiy <skiy@outlook.com>
2018-11-15 16:25:57 +0000

    added better loading and fixed some issues

--- _core/__init__.py
+++ _core/__init__.py
@@ -1,16 +1,25 @@
-def setlen(var="",length=1,type=0,fill="0"):
-    while length(var) < length:
+from . import module
+
+def getinfo(name=""):
+    with open(name+"/__init__.py","r") as m:
+        m=m.read()
+    name,ver,url,author=setlen(module.fetch("module.module(",")",["","","",""]),4)
+    depends=fetch("module.needs(",")",[""])
+    return name,ver,url,author,depends
+
+def setlen(var="",leng=1,tp=0,fl="0"):
+    while len(var) < leng:
         if list(var)==var:
-            if type==1:
+            if tp==1:
                 var=[]+var
             else:
                 var+=[]
         else:
-          if type==1
-              var=fill+str(var)
+          if tp==1:
+              var=fl+str(var)
           else:
-              var=str(var)+fill
-    return var[0:length]
+              var=str(var)+fl
+    return var[0:leng]
 
 def setup():
     module.module("LOaBIS Core","0.3.0")
--- _core/module.py
+++ _core/module.py
@@ -1,16 +1,13 @@
 def module(name="",version="",url="",author=""):
+    '''defines the module, allowing it to be loaded into loabis'''
     return True
-    
-def getinfo(name=""):
-    with open(name+"/__init__.py","r") as m:
-        m=m.read()
-    name,ver,url,author=setlen(fetch("module.module(",")/n",["","","",""]),4)
-    return name,ver,url,author
-    
-def fetch(text="",start="",end"/n",default=""):
+
+def needs(modules=[]):
+    return True
+
+def fetch(text="",start="",end="/n",default=""):
     try:
         tmp=text.split(start)[1].split(end)[0]
     except:
         tmp=default
-    except:
     return tmp.split(",")
