2a53f1f501ed29ea4c5ee81b8c824165e9bee5d3
Skye Owen-Lloyd-Walters <SK1Y101@users.noreply.github.com>
2018-11-15 14:27:04 +0000

    added module info loading

--- _core/__init__.py
+++ _core/__init__.py
@@ -1,4 +1,16 @@
-
+def setlen(var="",length=1,type=0,fill="0"):
+    while length(var) < length:
+        if list(var)==var:
+            if type==1:
+                var=[]+var
+            else:
+                var+=[]
+        else:
+          if type==1
+              var=fill+str(var)
+          else:
+              var=str(var)+fill
+    return var[0:length]
 
 def setup():
     module.module("LOaBIS Core","0.3.0")
