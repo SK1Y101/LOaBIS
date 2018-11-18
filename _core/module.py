f1ab8a04904cb8d768add781cce97673ebf9fd8b
Skye Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2018-11-18 21:19:12 +0000

    finalised dependancy checking and began looking at module version checking

--- _core/module.py
+++ _core/module.py
@@ -1,3 +1,5 @@
+import sys, subprocess
+from . import *
 def module(name="",version="",longname="",url="",author=""):
     '''defines the module, allowing it to be loaded into loabis'''
     return True
@@ -14,3 +16,39 @@ def fetch(text="",start="",end="/n",default=""):
     for x in range(min(len(default),len(tmp))):
         _tmp[x]=tmp[x]
     return _tmp
+
+def _elog(err="",say=1):
+    _log("Error: "+str(type(err))+"; "+str(err)+"\n")
+    if say:
+        _say("Software encountered an error, consider submitting log to developers","Error")
+
+def _say(txt="",cli="System"):
+    for x in str(cli+": "+txt)+"\n":
+        sys.stdout.write(x)
+        time.sleep(0.003)
+
+def _log(txt="",say=0):
+    y=open("log.txt","a")
+    y.write("["+str(datetime.now())+"] - "+txt[0].upper()+txt[1:]+"\n")
+    y.close()
+    if say:
+        _say(txt)
+
+def inst(mod=""):
+    modi="sudo "
+    if sys.platform=="win32":
+        modi=""
+    _log(str(mod)+" not found, installing",1)
+    try:
+        try:
+            subprocess.call(str(modi)+"pip3 install "+str(mod),shell=True)
+        except:
+            subprocess.call(str(modi)+"pip install "+str(mod),shell=True)
+    except Exception as e:
+        _elog(e)
+    if getdat(getpip(),mod):
+        _log("Successfully installed",1)
+        return 0
+    else:
+        _log("Unsuccessfull installation",1)
+        return 1
